0445ee5871519370fdaaf75c90a7b898cd04088b
PanWidmo <tomasz.baranww@gmail.com>
2022-01-16 19:50:27 +0100

    Created first test: adds donation to WOŚP (100zł, pocztowy24)

--- cypress/integration/siepomaga_spec.js
+++ cypress/integration/siepomaga_spec.js
@@ -1,5 +1,17 @@
 describe("SiePomaga", () => {
-  it("adds donation to cart", () => {
+  it("adds donation to WOŚP (100zł, pocztowy24)", () => {
     cy.visit("https://www.siepomaga.pl/wosp");
+    cy.contains("a", "Wesprzyj").click({ force: true });
+    cy.get("input[value='100']").click();
+    cy.get("input[name='payment[signature]']").type("Super Darczyńca");
+    cy.contains("button", "Do koszyka darowizn").click();
+    cy.contains("div.ui.green.circular.small.label", "1");
+    cy.contains("a", "Opłać koszyk darowizn").click();
+    cy.contains("div.sc-gUQvok.dYTsPi", "100 zł");
+    cy.get("input[name='cart[payer_email]']").type("test@gmail.com");
+    cy.contains("button", "Więcej opcji płatności").click();
+    cy.get("img[alt='Pocztowy24']").click();
+    cy.get("input[id='cart[accept_regulations]']").click({ force: true });
+    cy.contains("button", "Przejdź do płatności").click();
   });
 });
